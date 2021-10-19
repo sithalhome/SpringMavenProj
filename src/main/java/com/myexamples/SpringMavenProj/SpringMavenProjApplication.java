65d659c3879781f1539477d48b989f198325bce5
demouser <abc.google.com>
2021-10-19 11:20:43 -0400

    first message

---
+++ src/main/java/com/myexamples/SpringMavenProj/SpringMavenProjApplication.java
@@ -0,0 +1,13 @@
+package com.myexamples.SpringMavenProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myexamples/SpringMavenProj/SpringMavenProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexamples.SpringMavenProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
